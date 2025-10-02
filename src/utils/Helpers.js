a8244185484410e08082183442c2e69905f8f045
Guillaume Kuc <guillaume@guillaumekuc.com>
2025-10-02 17:32:52 +0200

    [added] passive/active system + basic Chord Relationship compute

---
+++ src/utils/Helpers.js
@@ -0,0 +1,13 @@
+export default class Helpers {
+
+  static assert(condition, message = "Assertion failed") {
+    if (!condition) {
+      throw new Error(message);
+    }
+  }
+
+  static arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    return arr1.every((val, index) => val === arr2[index]);
+  }
+}
