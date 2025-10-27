b9f1aca1f2a603587e7b8281525b1451bff0d25b
Guillaume Kuc <guillaume@guillaumekuc.com>
2025-10-27 20:17:06 +0100

    [update] gh-pages prep

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/chord-relationships-analyzer/',
   server: {
     port: 5173, // default port
     open: true // auto open browser
